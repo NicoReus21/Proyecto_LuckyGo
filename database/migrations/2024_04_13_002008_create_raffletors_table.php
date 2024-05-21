0ff0ba402af549d002ca63dc2895c28f88e71a84
D4ySito <146564158+D4ySito@users.noreply.github.com>
2024-05-21 02:26:24 -0400

    Implementación de seeder para sorteadores.

--- database/migrations/2024_04_13_002008_create_raffletors_table.php
+++ database/migrations/2024_04_13_002008_create_raffletors_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Correr migraciones.
      */
     public function up(): void
     {
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->integer('age');
             $table->string('password');
             $table->rememberToken();
@@ -22,41 +23,20 @@ return new class extends Migration
         });
 
         Schema::table('raffletors', function (Blueprint $table) {
-            //$table->dropColumn('email_verified_at');
-            $table->dropColumn('remember_token');
+        //    $table->dropColumn('email_verified_at');
+        //    $table->dropColumn('remember_token');
             $table->dropColumn('created_at');
             $table->dropColumn('updated_at');
             
         });
-/*
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
 
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });*/
     }
 
     /**
-     * Reverse the migrations.
+     * Revertir migraciones.
      */
     public function down(): void
     {
         Schema::dropIfExists('raffletors');
-        //Schema::dropIfExists('password_reset_tokens');
-        //Schema::dropIfExists('sessions');
-
-        $table->timestamp('email_verified_at')->nullable();
-        $table->rememberToken();
-        $table->created_at();
-        $table->updated_at();
     }
 };
