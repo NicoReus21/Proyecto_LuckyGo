85d462321485e4f15f66d31567e9e9c8b6527ddf
kingrxben <lulentoni40@gmail.com>
2024-06-02 02:56:13 -0400

    Corrección autenticadores, y sincronización entre ramas.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -108,7 +108,17 @@ class AuthController extends Controller
      */
     public function logout()
     {
-        auth()->logout();
+
+        
+        if (Auth::guard('admin')) {
+            auth('admin')->logout();
+        }
+
+        if (Auth::guard('raffletor')) {
+            auth('raffletor')->logout();
+        }
+
+        //auth('admin')->logout();
         return redirect()->route('loginForm');
     }
 }
